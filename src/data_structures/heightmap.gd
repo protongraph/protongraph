d135cbb0782eab77700220c6f785d93a637a41a3
Bryce Looms <bryce@spiffmedia.com>
2020-06-02 11:41:17 +1200

    Stopped using PoolRealArray and added util methods

--- src/data_structures/heightmap.gd
+++ src/data_structures/heightmap.gd
@@ -1,14 +1,24 @@
 extends Object
 class_name ConceptGraphHeightmap
 
-export var size: Vector2
-export var data: PoolRealArray
+var size: Vector2
+var data: Array
 
 func init(_size: Vector2) -> void:
 	size = _size
-	data = PoolRealArray()
+	data = Array()
 	data.resize(size.x * size.y)
 
 
-func get_data(x: int, y: int) -> float:
+func get_point(x: int, y: int) -> float:
 	return data[y * size.y + x]
+
+func set_point(x: int, y: int, height: float) -> void:
+	data[y * size.y + x] = height
+
+
+func get_index(i: int) -> float:
+	return data[i]
+
+func set_index(i: int, height: float) -> void:
+	data[i] = height
