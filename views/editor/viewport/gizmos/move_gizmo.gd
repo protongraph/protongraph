c7c24e03ea71ff1c399e55b46718daa3cba9f7a9
HungryProton <exb2800+github@protonmail.com>
2020-07-26 12:30:36 +0200

    move gizmo on a plane now works

--- views/editor/viewport/gizmos/move_gizmo.gd
+++ views/editor/viewport/gizmos/move_gizmo.gd
@@ -23,7 +23,10 @@ var _qz: Area
 
 var _is_dragging := false
 var _selected_axis := -1
+var _selected_plane := -1
 var _selected_node: Spatial
+var _click_position
+var _initial_position
 var _camera
 
 
@@ -32,9 +35,17 @@ func _ready() -> void:
 	_ay = get_node(arrow_y)
 	_az = get_node(arrow_z)
 
-	_ax.connect("input_event", self, "_on_input_event", [0])
-	_ay.connect("input_event", self, "_on_input_event", [1])
-	_az.connect("input_event", self, "_on_input_event", [2])
+	_qx = get_node(quad_x)
+	_qy = get_node(quad_y)
+	_qz = get_node(quad_z)
+
+	Signals.safe_connect(_ax, "input_event", self, "_on_input_event", [0])
+	Signals.safe_connect(_ay, "input_event", self, "_on_input_event", [1])
+	Signals.safe_connect(_az, "input_event", self, "_on_input_event", [2])
+
+	Signals.safe_connect(_qx, "input_event", self, "_on_input_event", [3])
+	Signals.safe_connect(_qy, "input_event", self, "_on_input_event", [4])
+	Signals.safe_connect(_qz, "input_event", self, "_on_input_event", [5])
 
 
 func _process(delta: float) -> void:
@@ -68,12 +79,17 @@ func _clear_state() -> void:
 		_selected_node = null
 
 
-func _on_input_event(camera: Node, event: InputEvent, click_position: Vector3, click_normal: Vector3, shape_idx: int, axis_idx: int):
+func _on_input_event(camera: Node, event: InputEvent, click_position: Vector3, _click_normal: Vector3, _shape_idx: int, handle_idx: int):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			_is_dragging = true
-			_selected_axis = axis_idx
 			_camera = camera
+			_click_position = click_position
+			_initial_position = global_transform.origin
+			if handle_idx < 3:
+				_selected_axis = handle_idx
+			else:
+				_selected_plane = handle_idx % 3
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -84,31 +100,66 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.button_index == BUTTON_LEFT and event.pressed == false:
 			_is_dragging = false
 			_selected_axis = -1
+			_selected_plane = -1
 			# TODO : handle undo redo
 			return
 
 	if event is InputEventMouseMotion:
-		var global_inverse: Transform = transform.affine_inverse()
 		var point = event.position
-
 		var ray_from = _camera.project_ray_origin(point)
 		var ray_dir = _camera.project_ray_normal(point)
 
+		# User dragged an arrow
+		if _selected_axis != -1:
+			var global_inverse: Transform = transform.affine_inverse()
 
-		var axis: Vector3
-		axis[_selected_axis] = 1.0
+			var axis: Vector3
+			axis[_selected_axis] = 1.0
 
-		var p1 = axis * 4096
-		var p2 = -p1
-		var g1 = global_inverse.xform(ray_from)
-		var g2 = global_inverse.xform(ray_from + ray_dir * 4096)
+			var p1 = axis * 4096
+			var p2 = -p1
+			var g1 = global_inverse.xform(ray_from)
+			var g2 = global_inverse.xform(ray_from + ray_dir * 4096)
 
-		var points = Geometry.get_closest_points_between_segments(p1, p2, g1, g2)
-		var d = points[0][_selected_axis] - 1.0
+			var points = Geometry.get_closest_points_between_segments(p1, p2, g1, g2)
+			var d = points[0][_selected_axis] - 1.0
 
-		translate_object_local(axis * d)
+			translate_object_local(axis * d)
+
+		# User dragged a quad
+		elif _selected_plane != -1:
+			var plane = _get_plane(_selected_plane)
+			var ray_hit_pos = plane.intersects_ray(ray_from, ray_dir)
+
+			if not ray_hit_pos:
+				return
+
+			var ignore_vec = Vector3.ZERO
+			ignore_vec[_selected_plane] = 1.0
+			ignore_vec = Vector3.ONE - ignore_vec
+
+			global_transform.origin = _initial_position + (ray_hit_pos - _click_position) * ignore_vec
+
 		_selected_node.transform.origin = transform.origin
 		emit_signal("gizmo_changed")
+
+
+func _get_plane(idx: int) -> Plane:
+	var o = get_global_transform().origin
+	var d
+	var n
+	match idx:
+		0:
+			n = Vector3.RIGHT
+			d = o.x
+		1:
+			n = Vector3.UP
+			d = o.y
+		2:
+			n = Vector3.BACK
+			d = o.z
+
+	return Plane(n, d)
 
 
 func _on_selected_node_changed(_node) -> void:
