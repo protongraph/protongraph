cb81519e44888b08c92bf18d65ccb7b5ba7438ce
HungryProton <exb2800+github@protonmail.com>
2020-08-01 10:36:49 +0200

    fixed compile_all.sh

--- src/native/compile_all.sh
+++ src/native/compile_all.sh
@@ -2,11 +2,8 @@
 
 platform=$1
 
-cd godot-cpp/godot_headers
+cd godot-cpp
 scons -j2 bits=64 generate_bindings=yes debug_symbols=no platform=$platform
-
-cd ../
-scons -j2 platform=$platform bits=64 debug_symbols=no
 
 cd ../thirdparty/mesh_optimizer
 scons -j2 platform=$platform bits=64 debug_symbols=no
